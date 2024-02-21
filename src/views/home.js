e0f925b70abbeba0b6435a47df6a6017ff0e1a28
Nabhan Mazid <129209117+nmazid121@users.noreply.github.com>
2024-02-21 18:43:35 -0500

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -117,7 +117,7 @@ const Home = (props) => {
             </div>
           </div>
           <div data-thq="thq-burger-menu" className="home-burger-menu">
-            <svg viewBox="0 0 1024 1024" className="home-icon02">
+            <svg viewBox="0 0 1024 1024" className="home-icon2">
               <path d="M64 192h896v192h-896zM64 448h896v192h-896zM64 704h896v192h-896z"></path>
             </svg>
           </div>
@@ -129,46 +129,43 @@ const Home = (props) => {
                   <br></br>
                 </span>
                 <div data-thq="thq-close-menu" className="home-close-menu">
-                  <svg viewBox="0 0 1024 1024" className="home-icon04">
+                  <svg viewBox="0 0 1024 1024" className="home-icon4">
                     <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
                   </svg>
                 </div>
               </div>
               <div className="home-buttons1">
-                <button className="home-about-us-mobile button">
+                <a href="#aboutUs" className="home-about-us-mobile button">
                   <span>
                     <span>About Us</span>
                     <br></br>
                   </span>
-                </button>
+                </a>
               </div>
               <div className="home-buttons2">
-                <button className="home-menu button">
+                <a href="#menu" className="home-menu button">
                   <span>
                     <span className="home-text13">Menu</span>
                     <br></br>
                   </span>
-                </button>
+                </a>
               </div>
               <div className="home-buttons3">
-                <button className="home-gallery button">
+                <a href="#gallery" className="home-gallery button">
                   <span>
                     <span className="home-text16">Gallery</span>
                     <br></br>
                   </span>
-                </button>
+                </a>
               </div>
-            </div>
-            <div>
-              <svg viewBox="0 0 950.8571428571428 1024" className="home-icon06">
-                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
-              </svg>
-              <svg viewBox="0 0 877.7142857142857 1024" className="home-icon08">
-                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
-              </svg>
-              <svg viewBox="0 0 602.2582857142856 1024" className="home-icon10">
-                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
-              </svg>
+              <div className="home-buttons4">
+                <a href="#gallery" className="home-contact-us button">
+                  <span>
+                    <span className="home-text19">Contact Us</span>
+                    <br></br>
+                  </span>
+                </a>
+              </div>
             </div>
           </div>
         </header>
@@ -176,7 +173,7 @@ const Home = (props) => {
       <div className="home-hero">
         <div className="home-container03">
           <h1 className="home-hero-heading heading1">
-            <span className="home-text18">Ara&apos;s Hot Chicken</span>
+            <span className="home-text21">Ara&apos;s Hot Chicken</span>
             <br></br>
           </h1>
           <span className="home-hero-sub-heading">
@@ -188,7 +185,7 @@ const Home = (props) => {
       <div id="aboutUs" className="home-details">
         <div className="home-details1">
           <div className="home-container04">
-            <span className="home-text20 sectionTitle">
+            <span className="home-text23 sectionTitle">
               <span>AbouT US</span>
               <br></br>
             </span>
@@ -303,21 +300,21 @@ const Home = (props) => {
             ></iframe>
             <div className="home-container11">
               <div className="home-container12">
-                <h1 className="home-text23">
+                <h1 className="home-text26">
                   <span>Phone Number</span>
                   <br></br>
                 </h1>
                 <button type="button" className="home-button button">
                   Call Now
                 </button>
-                <span className="home-text26">
+                <span className="home-text29">
                   <br></br>
                   <span>(732) 447-2571</span>
                   <br></br>
                 </span>
               </div>
               <div className="home-container13">
-                <h1 className="home-text30">
+                <h1 className="home-text33">
                   <span>Address</span>
                   <br></br>
                 </h1>
@@ -342,11 +339,11 @@ const Home = (props) => {
                 </span>
               </div>
               <div className="home-container14">
-                <h1 className="home-text43">
+                <h1 className="home-text46">
                   <span>Business Hours</span>
                   <br></br>
                 </h1>
-                <span className="home-text46">
+                <span className="home-text49">
                   <br></br>
                   <span>Mon: Closed</span>
                   <br></br>
@@ -398,8 +395,8 @@ const Home = (props) => {
             <div className="home-container17">
               <div className="home-container18">
                 <div className="home-container19">
-                  <span className="home-text67">
-                    <span className="home-text68">
+                  <span className="home-text70">
+                    <span className="home-text71">
                       © 2023 Aras Hot Chicken, All Rights Reserved.
                     </span>
                     <br></br>
@@ -411,14 +408,14 @@ const Home = (props) => {
             </div>
           </div>
         </footer>
-        <div className="home-icon-group1">
+        <div className="home-icon-group">
           <a
             href="https://www.instagram.com/arashotchicken/"
             target="_blank"
             rel="noreferrer noopener"
             className="home-link4"
           >
-            <svg viewBox="0 0 877.7142857142857 1024" className="home-icon12">
+            <svg viewBox="0 0 877.7142857142857 1024" className="home-icon6">
               <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
             </svg>
           </a>
