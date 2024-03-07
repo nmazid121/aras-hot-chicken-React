7d6bcec7c506f0199700e68ba49db6e2218ed91a
nmazid121 <nmazid121@gmail.com>
2024-03-06 19:13:27 -0500

    fixed

--- src/views/home.js
+++ src/views/home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import { Helmet } from 'react-helmet'
 
@@ -7,6 +7,12 @@ import GalleryCard3 from '../components/gallery-card3'
 import './home.css'
 
 const Home = (props) => {
+
+  useEffect(() => {
+    window.location.hash = '#homeHeader';
+  }, []);
+
+  
   return (
     <div className="home-container">
       <Helmet>
