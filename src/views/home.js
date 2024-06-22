bf6f1f6a972f4376a6318db151752f42f49836f0
nmazid121 <nmazid121@gmail.com>
2024-06-22 16:44:17 -0400

    d

--- src/views/home.js
+++ src/views/home.js
@@ -208,11 +208,11 @@ const Home = (props) => {
           <div className="home-blog">
             <div className="home-container04">
               <BlogPostCard2
-                label="Ramadan 2024"
-                title="Ramadan Mubarak"
+                label="Closed Today"
+                title="Closed Today Due to AC"
                 imageSrc="https://media.istockphoto.com/id/1305026622/photo/ramadan-kareem-lantern-and-dates-fruit-with-city-light-background.jpg?s=612x612&amp;w=0&amp;k=20&amp;c=lhfwznUVfslOZoY06stNxKkn9Ttl_jAQhpWII_QlJAc="
                 profileSrc="https://images.unsplash.com/photo-1611232658409-0d98127f237f?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDIzfHxwb3J0cmFpdCUyMHdvbWFufGVufDB8fHx8MTYyNjQ1MDU4MQ&amp;ixlib=rb-1.2.1&amp;h=1200"
-                description="AssalamuAlaikum Dear Customers! Please stop by and join us to break your fasts with complimentary Dates and Water during Ramadan 2024. No purchase necessary"
+                description=""
                 rootClassName="rootClassName3"
               ></BlogPostCard2>
             </div>
@@ -263,7 +263,7 @@ const Home = (props) => {
           <div className="home-container08">
             <div className="home-container09">
               <img
-                alt="pastedImage" 
+                alt="pastedImage"
                 src="/external/arasMenuVertical-3-11-1.png"
                 className="home-pasted-image1"
               />
